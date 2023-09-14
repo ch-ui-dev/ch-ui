7336344ae11cba48befcdc69c06797a87b699978
thure <will@braneframe.com>
2023-09-14 01:11:13 -0700

    Create scratch project.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="p-2 mli-auto max-is-[60rem]">
+      <h1 className="text-lg font-semibold">Hello world!</h1>
+    </main>
+  );
+}
+
+export default App;
