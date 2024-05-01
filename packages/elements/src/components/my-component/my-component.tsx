98868feb55f37419957778d01f2db1cb47d5f516
thure <will@braneframe.com>
2024-04-30 23:17:41 -0700

    Move Storybook to `utils`.

--- packages/elements/src/components/my-component/my-component.tsx
+++ packages/elements/src/components/my-component/my-component.tsx
@@ -5,7 +5,7 @@ import { Component, Prop, h } from '@stencil/core';
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
-  shadow: true,
+  shadow: false,
 })
 export class MyComponent {
   /**
