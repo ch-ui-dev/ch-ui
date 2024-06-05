aa20a5742aa9fd0ef382cd72bf9fab32313818b9
thure <will@braneframe.com>
2024-06-05 02:32:19 -0700

    Fix storybook theme.

--- apps/stories/.storybook-shared/preview.ch-t.ts
+++ apps/stories/.storybook-shared/preview.ch-t.ts
@@ -19,7 +19,10 @@ const physicalColors: PhysicalColorTokensConfig = {
 };
 
 const semanticColors: SemanticColorTokensConfig<typeof physicalColors> = {
-  themes: { light: null, dark: '@media (prefers-color-scheme: dark)' },
+  themes: {
+    light: [':root'],
+    dark: ['@media (prefers-color-scheme: dark)', ':root'],
+  },
   semanticColors: {
     'ch-bg-input': {
       light: '--primary-100',
