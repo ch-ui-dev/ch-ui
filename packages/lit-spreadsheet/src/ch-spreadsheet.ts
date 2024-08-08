a59e4981283cefa404e7cf5ad66f6967c65c0806
thure <will@braneframe.com>
2024-08-07 18:51:04 -0700

    Populate grid with cells. Listen to `wheel` events.

--- packages/lit-spreadsheet/src/ch-spreadsheet.ts
+++ packages/lit-spreadsheet/src/ch-spreadsheet.ts
@@ -4,40 +4,103 @@ import { LitElement, html } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { ref, createRef, Ref } from 'lit/directives/ref.js';
 
+const colwidth = 64;
+
+const rowheight = 20;
+
+function toColName(n: number): string {
+  return `C${n}`;
+}
+function toRowName(n: number): string {
+  return `${n}`;
+}
+
 @customElement('ch-spreadsheet')
 export class ChSpreadsheet extends LitElement {
   @state()
-  position = { x: 0, y: 0 };
+  posInline = 0;
+  @state()
+  posBlock = 0;
 
   @state()
-  size = { inlineSize: 0, blockSize: 0 };
+  sizeInline = 0;
+  @state()
+  sizeBlock = 0;
 
   @state()
   observer = new ResizeObserver((entries) => {
-    this.size = entries?.[0]?.contentBoxSize?.[0] ?? {
+    const { inlineSize, blockSize } = entries?.[0]?.contentBoxSize?.[0] ?? {
       inlineSize: 0,
       blockSize: 0,
     };
+    this.sizeInline = inlineSize;
+    this.sizeBlock = blockSize;
   });
 
   viewportRef: Ref<HTMLDivElement> = createRef();
 
+  handleWheel = ({ deltaX, deltaY }: WheelEvent) => {
+    console.log('[wheel]', deltaX, deltaY);
+  };
+
   override render() {
+    const visibleCols = Math.ceil(
+      (this.sizeInline + this.posInline) / colwidth,
+    );
+    const visibleRows = Math.ceil((this.sizeBlock + this.posBlock) / rowheight);
     return html`<div role="none" class="ch-spreadsheet">
       <div role="none" class="ch-spreadsheet__corner"></div>
-      <div role="none" class="ch-spreadsheet__fixed-row">Column headings</div>
+      <div role="none" class="ch-spreadsheet__columnheader">
+        ${[...Array(visibleCols)].map((_, i) => {
+          return html`<div
+            role="gridcell"
+            style="inline-size:${colwidth}px;block-size:${rowheight}px;"
+          >
+            ${toColName(i)}
+          </div>`;
+        })}
+      </div>
       <div role="none" class="ch-spreadsheet__corner"></div>
-      <div role="none" class="ch-spreadsheet__fixed-column">123</div>
+      <div role="none" class="ch-spreadsheet__rowheader">
+        ${[...Array(visibleRows)].map((_, j) => {
+          return html`<div role="gridcell" style="block-size:${rowheight}px;">
+            ${toRowName(j)}
+          </div>`;
+        })}
+      </div>
       <div
         role="none"
         class="ch-spreadsheet__viewport"
+        @wheel="${this.handleWheel}"
         ${ref(this.viewportRef)}
       >
-        Size: ${this.size.inlineSize}, ${this.size.blockSize}
+        <div role="grid" class="ch-spreadsheet__content">
+          ${[...Array(visibleCols)].map((_, i) => {
+            return [...Array(visibleRows)].map((_, j) => {
+              return html`<div
+                role="gridcell"
+                style="inline-size:${colwidth}px;block-size:${rowheight}px;grid-column:${i +
+                1}/${i + 2};grid-row:${j + 1}/${j + 2}"
+              ></div>`;
+            });
+          })}
+        </div>
       </div>
-      <div role="none" class="ch-spreadsheet__scrollbar--vertical">X</div>
+      <div
+        role="none"
+        class="ch-spreadsheet__scrollbar"
+        aria-orientation="vertical"
+      >
+        <div role="none" class="ch-spreadsheet__scrollbar__thumb"></div>
+      </div>
       <div role="none" class="ch-spreadsheet__corner"></div>
-      <div role="none" class="ch-spreadsheet__scrollbar--horizontal">X</div>
+      <div
+        role="none"
+        class="ch-spreadsheet__scrollbar"
+        aria-orientation="horizontal"
+      >
+        <div role="none" class="ch-spreadsheet__scrollbar__thumb"></div>
+      </div>
       <div role="none" class="ch-spreadsheet__corner"></div>
     </div>`;
   }
